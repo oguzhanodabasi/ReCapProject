77aaaf4dedd09cb61a5ee4639648ae790e4c1f6a
cokelek <modaozi70@gmail.com>
2022-08-22 16:11:20 +0300

    Entity Framework infrastructure has been prepared. GetCarsByBrandId and GetCarsByColorId services have been written.

--- DataAccess/Abstract/ICarDal.cs
+++ DataAccess/Abstract/ICarDal.cs
@@ -9,10 +9,5 @@ namespace DataAccess.Abstract
 {
     public interface ICarDal
     {
-        List<Car> GetById(int carId);
-        List<Car> GetAll();
-        void Add(Car car);
-        void Update(Car car);
-        void Delete(Car car);
     }
 }
